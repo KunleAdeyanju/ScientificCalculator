b8a09b8958cbfdbf5e4d2a991e386f87da08fc06
Kunle <kunle@Zipcoders-MacBook-Pro-5.local>
2025-03-01 16:47:21 -0500

    I think the user interface is 92% setup

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -10,21 +10,25 @@ public class MainApplication {
         // terminate program
         int runTime = 1;
 
+        // variable used to track the state of the program
         double previousCalculationDouble = 0;
         int storeAnswer = 0;
         double oldAnswer = 0;
-        //int previousCalculationInt;
+        boolean isAnswerStored = false;
 
         Console.println("Welcome to my calculator!");
+        // keep the program operating until user signs they are done
         do {
             String s = Console.getStringInput("What would you like to do");
 
 
+            // cycles through the calculation available to the user
             switch (s.toLowerCase()){
                 case "quit":
                     runTime = 0;
                     break;
                 case "add":
+                    // runs correct script dependent on if there is a previous answer stored
                     if (storeAnswer == 0) {
                         double xAdd = Console.getDoubleInput("Enter number 1: ");
                         double yAdd = Console.getDoubleInput("Enter number 2: ");
@@ -36,6 +40,7 @@ public class MainApplication {
                         previousCalculationDouble = coreFunctions.addition(x2, yAdd2);
                         Console.println(x2+ " + " + yAdd2 + " = " + Console.resultsFormat(previousCalculationDouble));
                     }
+                    isAnswerStored = true;
                     break;
                 case "square":
                     if (storeAnswer == 0) {
@@ -47,6 +52,7 @@ public class MainApplication {
                         previousCalculationDouble = coreFunctions.square(xSquare2);
                         Console.println(xSquare2 + " squared is " + Console.resultsFormat(previousCalculationDouble));
                     }
+                    isAnswerStored = true;
                     break;
                 case "square root":
                     if (storeAnswer == 0) {
@@ -58,7 +64,9 @@ public class MainApplication {
                         previousCalculationDouble = coreFunctions.squareRoot(xsr2);
                         Console.println("The square root of " + xsr2 + " is " + Console.resultsFormat(previousCalculationDouble));
                     }
+                    isAnswerStored = true;
                     break;
+                // in case of typo return to beginning of the loop
                 default:
                     Console.println("That wasn't an option");
                     if(storeAnswer == 1){
@@ -67,70 +75,32 @@ public class MainApplication {
                     }
             }
 
-            if ( previousCalculationDouble > 0) {
+            // only offer option to store answer if there is an answer to store
+            // in every case variable are reset
+            if ( isAnswerStored) {
                 String a = Console.getStringInput("Do you wish to use wish to store the answer ? (Y/N)");
                 if (a.equalsIgnoreCase("y")) {
                     storeAnswer = 1;
                     oldAnswer = previousCalculationDouble;
                     previousCalculationDouble = 0;
                     Console.println(" Your stored answer is " + Console.resultsFormat(oldAnswer));
+                    isAnswerStored = false;
                 } else if (a.equalsIgnoreCase("n")) {
                     storeAnswer = 0;
                     previousCalculationDouble = 0;
+                    isAnswerStored = false;
                 } else if (a.equalsIgnoreCase("quit")){
                     runTime = 0;
                 }else {
                     Console.println("That wasn't an option, sorry data is lost please start again");
                     previousCalculationDouble = 0;
                     storeAnswer = 0;
+                    isAnswerStored = false;
 
 
                 }
             }
 
-
-            /*
-            if(s.equalsIgnoreCase("quit")){
-                runTime = 0;
-            }else if (s.equalsIgnoreCase("add")){
-                double x = Console.getDoubleInput("Enter number 1: ");
-                double y = Console.getDoubleInput("Enter number 2: ");
-                previousCalculationDouble = coreFunctions.addition(x,y);
-                Console.println(y + " + " + x + " = " + previousCalculationDouble);
-            } else if (s.equalsIgnoreCase("square")) {
-                double x = Console.getDoubleInput("Enter number 1: ");
-                previousCalculationDouble = coreFunctions.square(x);
-                Console.println(x + " squared is " +previousCalculationDouble);
-            }
-
-
-
-            String a = Console.getStringInput("Do you wish to use wish to use answer");
-            if(a.equalsIgnoreCase("yes")){
-                double newX = previousCalculationDouble;
-                String s = Console.getStringInput("What would you like to do");
-                if(s.equalsIgnoreCase("quit")){
-                    runTime = 0;
-                }else if (s.equalsIgnoreCase("add")){
-                    double y = Console.getDoubleInput("Enter number 2: ");
-                    previousCalculationDouble = coreFunctions.addition(newX,y);
-                    Console.println(y + " + " + newX + " = " + previousCalculationDouble);
-                } else if (s.equalsIgnoreCase("square")) {
-                    double x = Console.getDoubleInput("Enter number 1: ");
-                    previousCalculationDouble = coreFunctions.square(newX);
-                    Console.println(x + " squared is " +previousCalculationDouble);
-                }
-            }
-            */
-
-            /*Integer i = Console.getIntegerInput("Enter an integer");
-            Double d = Console.getDoubleInput("Enter a double.");
-
-            Console.println("The user input %s as a string", s);
-            Console.println("The user input %s as a integer", i);
-            Console.println("The user input %s as a d", d);
-
-             */
         }while (runTime == 1);
     }
 }
