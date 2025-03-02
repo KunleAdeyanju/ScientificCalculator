ff7cadbce4b84cba14f5fd5104ed11da5127df96
Kunle <kunle@Zipcoders-MacBook-Pro-5.local>
2025-03-02 11:44:00 -0500

    added ScientificFetures class

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/ScientificFetures.java
@@ -0,0 +1,13 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class ScientificFetures {
+    // arguments class CoreFunctions takes
+    double x, y;
+
+    ScientificFetures(double a, double b) {
+        x = a;
+        y = b;
+    }
+}
+
+
